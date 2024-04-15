a939537bac38d18352ab43299f1da41383d89553
Olga Molkova <olgamolkova190719@gmail.com>
2024-04-15 20:00:21 +0300

    step2 changed Object to <T>

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -4,39 +4,39 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
-public class StringSchema {
+public class StringSchema<T> {
 
     protected boolean required;
-    Map<String, Predicate> predicates = new LinkedHashMap();
+    Map<String, Predicate<T>> predicates = new LinkedHashMap();
 
 
-    public StringSchema required() {
+    public StringSchema<T> required() {
         addPredicate("required", s -> s instanceof String && !s.equals("") && s != null);
         required = true;
         return this;
     }
 
-    public StringSchema contains(String subString) {
+    public StringSchema<T> contains(String subString) {
         addPredicate("contains", s -> s.toString().contains(subString));
         return this;
     }
 
-    public StringSchema minLength(int number) {
+    public StringSchema<T> minLength(int number) {
         addPredicate("minLength", n -> n.toString().length() >= number);
         return this;
     }
-    public boolean isValid(Object obj) {
-        if (obj == null || obj == "") {
+    public boolean isValid(T value) {
+        if (value == null || value == "") {
             return !required;
         }
         for (var predicate : predicates.entrySet()) {
-            if (!predicate.getValue().test(obj)) {
+            if (!predicate.getValue().test(value)) {
                 return false;
             }
         }
         return true;
     }
-    public void addPredicate(String name, Predicate predicate) {
+    public void addPredicate(String name, Predicate<T> predicate) {
         predicates.put(name, predicate);
     }
 }
