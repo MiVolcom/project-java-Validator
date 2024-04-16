f5f82c1802ab3a242c08c9284928915758cf73d6
Olga Molkova <olgamolkova190719@gmail.com>
2024-04-16 23:16:58 +0300

    fix for hexletCheck

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,11 +1,9 @@
-package hexlet.code;
 
+package hexlet.code;
 import hexlet.code.schemas.MapSchema;
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
-
 public final class Validator {
-    private Validator validator;
     public StringSchema string() {
         return new StringSchema();
     }
